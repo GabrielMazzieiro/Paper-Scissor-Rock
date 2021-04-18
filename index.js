f1fea5b981bb26ae34366a01e1a3c2582f59bea3
Gabriel Mazzieiro <gabrielmazzieiro@pop-os.localdomain>
2021-04-18 18:43:05 -0300

    restart done

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ const paperID = $("#paper").attr("id");
 const scissorsID = $("#scissors").attr("id");
 const rockID = $("#rock").attr("id");
 
+let score = 0;
+let chosenOption;
+const gameOption = ["paper", "scissors", "rock"];
 
-var chosenOption;
-const gameOption = ['paper','scissors','rock']
+const innerTimeOut = 1000;
+const outerTimeOut = innerTimeOut * 2;
 
 // Rules Button
 
@@ -25,151 +28,181 @@ $(".btn-close").click(() => {
   $("#rules-bg").css("display", "none");
 });
 
-$(".circle").click((e) => {
-  chosenOption = $(e.currentTarget).attr("id");
-});
-
-
-function houseCircle (){
-  $('#'+houseChoice+'-chosen').removeClass("hide").addClass("house-choice")
+function houseCircle() {
+  $("#" + houseChoice + "-chosen")
+    .removeClass("hide")
+    .addClass("house-choice");
 }
 
-function notChosen (){
+function notChosen() {
   $("h2").removeClass("hide");
   $("#wait-circle").removeClass("hide");
-
 }
 
+function handleWin() {
+  $("#final").text("you win!");
+  score++;
+  setTimeout(() => {
+    $(".score-number").text(score);
+  }, innerTimeOut * 4);
+}
 
+function handleLoss() {
+  $("#final").text("you lose!");
+}
+
+function handleTie() {
+  $("#final").text("it's a tie!");
+}
+
+function animation(choiceID) {
+  setTimeout(() => {
+    notChosen();
+  }, innerTimeOut);
+
+  setTimeout(() => {
+    houseCircle();
+    $("#wait-circle").addClass("hide");
+    setTimeout(() => {
+      $(".house-choice").addClass("end-game");
+      $("h2").addClass("end-game");
+      choiceID.addClass("end-game");
+      setTimeout(() => {
+        $("#game-result").removeClass("hide");
+      }, innerTimeOut);
+    }, innerTimeOut);
+  }, outerTimeOut);
+}
+
+function restartAnimation() {
+  $("#" + houseChoice + "-chosen")
+    .addClass("hide")
+    .removeClass("house-choice");
+  $("#game-result").addClass("reset");
+  $("#house-pick").addClass("reset");
+  $("#your-pick").addClass("reset");
+
+  setTimeout(() => {
+    $("#game-result").addClass("hide");
+    $("#game-result").removeClass("reset");
+    $("#your-pick").addClass("hide");
+    $("#your-pick").removeClass("reset");
+    $("#house-pick").addClass("hide");
+    $("#house-pick").removeClass("reset");
+  }, 500);
+
+  $("#wait-circle").addClass("hide");
+
+  $(".house-choice").removeClass("end-game");
+  $("h2").removeClass("end-game");
+  rock.removeClass("end-game");
+  paper.removeClass("end-game");
+  scissors.removeClass("end-game");
+}
+
+$(".play-again").click(() => {
+  restartAnimation();
+  $(".circle").attr("role", "button");
+  scissors.removeClass("hide");
+  paper.removeClass("hide");
+  rock.removeClass("hide");
+  triangle.removeClass("hide");
+  paper.removeClass("chosen");
+  scissors.removeClass("chosen");
+  rock.removeClass("chosen");
+  initGame();
+  $("#rock-chosen").removeClass("end-game");
+  $("#paper-chosen").removeClass("end-game");
+  $("#scissors-chosen").removeClass("end-game");
+});
 
 // Game Animation
+function initGame() {
+  $(".circle").click((e) => {
+    chosenOption = $(e.currentTarget).attr("id");
+    // Tirar a função de click das imagens
+    $(".circle").off("click").removeAttr("role");
 
-$(".circle").click(() => {
-  // Tirar a função de click das imagens 
-  $(".circle").off("click").removeAttr("role");
+    houseChoice = Math.floor(Math.random() * 3) + 1;
+    houseChoice = gameOption[houseChoice - 1];
+    console.log(houseChoice);
 
-
-  houseChoice = Math.floor(Math.random() * 3 ) + 1;
-  houseChoice = gameOption[houseChoice - 1];
-  console.log(houseChoice)
-  
-  switch (chosenOption) {
-    case paperID:
-
-    // Regras do jogo para a opção escolhida
-      switch (houseChoice){
-        case 'scissors':
-          $("#final").innerHTML = "you lose!";
-        break;
-        case 'paper':
-          $("#final").innerHTML = "it's a tie";
-          break;
-          case 'rock':
-            $("#final").innerHTML = "you win!";
+    switch (chosenOption) {
+      case paperID:
+        // Regras do jogo para a opção escolhida
+        switch (houseChoice) {
+          case "scissors":
+            handleLoss();
             break;
-      }
+          case "paper":
+            handleTie();
+            break;
+          case "rock":
+            handleWin();
+            break;
+        }
 
-     // Esconder as opções não escolhidas
-      scissors.addClass("hide");
-      rock.addClass("hide");
-      triangle.addClass("hide");
-      paper.addClass("chosen");
-  
-      // começar a animação das opções não escolhidas
-      setTimeout(() => {      
-        notChosen ()
-      }, 1000);
+        // Esconder as opções não escolhidas
+        scissors.addClass("hide");
+        rock.addClass("hide");
+        triangle.addClass("hide");
+        paper.addClass("chosen");
 
-      setTimeout(()=>{
-        houseCircle ()
-        $("#wait-circle").addClass("hide")
-        setTimeout(()=>{
-          $(".house-choice").addClass("end-game")
-          $("h2").addClass("end-game")
-          $("#paper").addClass("end-game")          
-       setTimeout(()=>{
-         $("#game-result").removeClass("hide")
-       },1000) 
-      },1000)
-      }, 2000);
+        // começar a animação das opções não escolhidas
+        animation(paper);
 
-      break;
-    case scissorsID :
-// Esconder as opções não escolhidas
-      paper.addClass("hide");
-      rock.addClass("hide");
-      triangle.addClass("hide");
-      scissors.addClass("chosen");
-  
-      // começar a animação das opções não escolhidas
-      setTimeout(() => {
-        notChosen ()
-      }, 1000);
-
-
-      setTimeout(()=>{
-        houseCircle ()
-        $("#wait-circle").addClass("hide")
-        setTimeout(()=>{
-          $(".house-choice").addClass("end-game")
-          $("h2").addClass("end-game")
-          $("#scissors").addClass("end-game")          
-       setTimeout(()=>{
-         $("#game-result").removeClass("hide")
-       },1000) 
-      },1000)
-      }, 2000)
-
-      // Regras do jogo para a opção escolhida
-    switch (houseChoice){
-      case 'scissors':
-      break;
-      case 'paper':
         break;
-        case 'rock':
-          break;
-    }
+      case scissorsID:
+        // Esconder as opções não escolhidas
+        paper.addClass("hide");
+        rock.addClass("hide");
+        triangle.addClass("hide");
+        scissors.addClass("chosen");
 
-    break;
+        // começar a animação das opções não escolhidas
+        animation(scissors);
 
-    case rockID :
-      // Esconder as opções não escolhidas
-      paper.addClass("hide");
-      scissors.addClass("hide");
-      triangle.addClass("hide");
-      rock.addClass("chosen");
-  
-      // começar a animação das opções não escolhidas
-      setTimeout(() => {
-        notChosen ()
-      }, 1000);
-      
-      setTimeout(()=>{
-        houseCircle ()
-        $("#wait-circle").addClass("hide")
-        setTimeout(()=>{
-          $(".house-choice").addClass("end-game")
-          $("h2").addClass("end-game")
-          $("#rock").addClass("end-game")          
-       setTimeout(()=>{
-         $("#game-result").removeClass("hide")
-       },1000) 
-      },1000)
-      }, 2000)
-       // Regras do jogo para a opção escolhida
-    switch (houseChoice){
-      case 'scissors':
-      break;
-      case 'paper':
+        // Regras do jogo para a opção escolhida
+        switch (houseChoice) {
+          case "scissors":
+            handleTie();
+            break;
+          case "paper":
+            handleWin();
+
+            break;
+          case "rock":
+            handleLoss();
+            break;
+        }
+
         break;
-        case 'rock':
-          break;
-    }
 
-    break;
-  }  
-  
-  // } else {
-  //   alert("deu errado cara");
-  // }
-});
+      case rockID:
+        // Esconder as opções não escolhidas
+        paper.addClass("hide");
+        scissors.addClass("hide");
+        triangle.addClass("hide");
+        rock.addClass("chosen");
+
+        // começar a animação das opções não escolhidas
+        animation(rock);
+        // Regras do jogo para a opção escolhida
+        switch (houseChoice) {
+          case "scissors":
+            handleLoss();
+            break;
+          case "paper":
+            handleWin();
+
+            break;
+          case "rock":
+            handleTie();
+            break;
+        }
+
+        break;
+    }
+  });
+}
+initGame();
